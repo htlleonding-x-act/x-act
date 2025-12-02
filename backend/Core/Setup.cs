8d414e46cfae2bfc527188f2c1f633e7d51bffd8
Mark Grünzweil <mark.gruenzweil@gmail.com>
2025-12-02 09:15:23 +0100

    feat: Add initial backend structure

---
+++ backend/Core/Setup.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using NodaTime.Serialization.SystemTextJson;
+
+namespace XAct.Core;
+
+public static class Setup
+{    
+    public static void RegisterServices(this IServiceCollection services)
+    {
+        services.AddSingleton<IClock>(SystemClock.Instance);
+    }
+
+    public static void ConfigureServices(this IServiceCollection services, bool isDevelopment)
+    {
+        services.ConfigureHttpJsonOptions(options =>
+        {
+            options.SerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+            options.SerializerOptions.WriteIndented = isDevelopment;
+            options.SerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.Never;
+            options.SerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            options.SerializerOptions.ConfigureForNodaTime(DateTimeZoneProviders.Tzdb);
+        });
+    }
+    
+    public static void ConfigureCors(this IServiceCollection services)
+    {
+        services.AddCors(options =>
+        {
+            options.AddPolicy(Const.CorsPolicyName, policy =>
+            {
+                policy.WithOrigins("http://localhost:4200")
+                      .AllowAnyMethod()
+                      .AllowAnyHeader();
+            });
+        });
+    }
+}
